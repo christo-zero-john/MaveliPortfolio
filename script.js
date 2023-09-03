b31f727a75d30e29cac0ae26113891008ad6abb5
Christo John <129399273+christo-zero-john@users.noreply.github.com>
2023-09-03 22:40:51 +0530

    Coomit 8

--- script.js
+++ script.js
@@ -4,22 +4,31 @@ var theLegend = document.getElementById("theLegend");
 var portfolio = document.getElementById("portfolio");
 var resources = document.getElementById("resources");
 var gallery = document.getElementById("gallery");
+var footer = document.getElementById("footer");
 
-
-
-nav.innerHTML = `<nav class="navbar navbar-expand-md navbar-light bg-transparent">
-
+var navBar = `<nav class="navbar navbar-expand-md navbar-light bg-transparent">
 
 <button class="navbar-toggler btn-outline-warning" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
 </button>
+
 <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
   <div class="navbar-nav mr-auto ml-auto">
     <a class="nav-item font-weight-bold text-warning nav-link" href="index.html" id="home">Home </a>
     <a class="nav-item font-weight-bold text-warning nav-link" href="legend.html" id="theLegend">The Legend</a>
-    <a class="nav-item font-weight-bold text-warning nav-link" href="portfolio.html" id="portfolio">Portfolio</a>
-    <a class="nav-item font-weight-bold text-warning nav-link" href="resources.html" id="resources">Resources</a>
-    <a class="nav-item font-weight-bold text-warning nav-link" href="gallery.html" id="gallery">Gallery</a>
+    <a class="nav-item font-weight-bold text-warning nav-link" href="biography.html" id="portfolio">Biography</a>
+    <a class="nav-item font-weight-bold text-warning nav-link" href="myth.html" id="resources">The Myth</a>
+    <a class="nav-item font-weight-bold text-warning nav-link" href="onam.html" id="gallery">Onam</a>
   </div>
 </div>
 </nav>`;
+
+var footerTxt = `<div class="footer rajdhani">
+<div class="ftDiv">
+    <span class="ft ">Copyright </span> <span class="tesNic orbitron">
+        Tesnic.io</span> <span class="ft">2023</span>
+</div>`
+
+footer.innerHTML = footerTxt;
+nav.innerHTML = navBar;
+
